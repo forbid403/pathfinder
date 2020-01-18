6f9031e293bb85b8673bb8bf83d35e4194aa80e6
forbid403 <forbid403@naver.com>
2020-01-19 00:09:22 +0900

    [set] database

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const db = mongoose.connection
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/api/hello', (req, res) => {
-res.send({ message: 'Hello Express!' })});
+app.get('/api/crawl/Atcoder', (req, res) => {
+
+});
+
+db.once('open', ()=>{console.log("connected to mongo db")})
+mongoose.connect('mongodb://localhost/pathfinder');
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ models/contest.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const contestSchema = new Schema({
+    site : {type : String, required : true},
+    title : {type : String, required : true},
+    startTime : {type : Date, required : true},
+    duration : {type : String, required : true},
+    url : {type : String, required : true, unique : true}
+})
+
+module.exports = mongoose.model('contest', contestSchema)
