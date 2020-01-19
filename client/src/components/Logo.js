b227d60c05b50c8933e5667e2d7cc5b149c6ac48
forbid403 <forbid403@naver.com>
2020-01-19 22:36:29 +0900

    aligned

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import logo from '../images/logo_black.png';
+import './Logo.css'
 
 class Logo extends Component{
-
     render()
     {
-        return <img src = {logo}
-        height = "150rem"
-        className = "center"/>;
+        return <img src = {logo} alt = "logo"
+        className = "logo"/>;
     }
 }
 
