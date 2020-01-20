aa9c4d78f2408eb684f4d815981a1793f4b5c17f
unknown <mile3880@gmail.com>
2020-01-21 00:08:42 +0900

    [create] new date range picker

--- client/src/components/PopUpPicker.js
+++ client/src/components/PopUpPicker.js
@@ -1,56 +1,81 @@
-import React, { Component } from 'react';
-import { format, addDays } from 'date-fns';
-import './PopUpPicker.css';
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
-import { DateRangePicker } from 'react-date-range';
+import React from 'react';
+import Helmet from 'react-helmet';
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
-class PopUpPicker extends Component {
+export default class PopUpPicker extends React.Component {
+  static defaultProps = {
+    numberOfMonths: 2,
+  };
 
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+    this.state = this.getInitialState();
+  }
 
-		this.state = {
-			dateRangePicker: {
-			  selection: {
-				startDate: new Date(),
-				endDate: null,
-				key: 'selection',
-			  },
-			}
-		}
-	}
+  getInitialState() {
+    return {
+      from: undefined,
+      to: undefined,
+    };
+  }
 
-	formatDateDisplay(date, defaultText) {
-		if (!date) return defaultText;
-		return format(date, 'YYYY-MM-DD');
-	}
+  handleDayClick(day) {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range);
+  }
 
-	handleRangeChange(which, payload) {
-		console.log(which, payload);
-		this.setState({
-		  [which]: {
-			...this.state[which],
-			...payload,
-		  },
-		});
-	}
+  handleResetClick() {
+    this.setState(this.getInitialState());
+  }
 
-	render(){
-		const {formatDateDisplay} = this;
-		
-		return (
-			<div>
-            <DateRangePicker
-              onChange={this.handleRangeChange.bind(this, 'dateRangePicker')}
-              showSelectionPreview={true}
-              moveRangeOnFirstSelection={false}
-              className={'PreviewArea'}
-              ranges={[this.state.dateRangePicker.selection]}
-            />
-		  </div>
-		)
-	}
+  render() {
+    const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
+    return (
+      <div className="RangeExample">
+        <p>
+          {!from && !to && 'Please select the first day.'}
+          {from && !to && 'Please select the last day.'}
+          {from &&
+            to &&
+            `Selected from ${from.toLocaleDateString()} to
+                ${to.toLocaleDateString()}`}{' '}
+          {from && to && (
+            <button className="link" onClick={this.handleResetClick}>
+              Reset
+            </button>
+          )}
+        </p>
+        <DayPicker
+          className="Selectable"
+          numberOfMonths={this.props.numberOfMonths}
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+        />
+        <Helmet>
+          <style>{`
+  .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+    background-color: #f0f8ff !important;
+    color: #4a90e2;
+  }
+  .Selectable .DayPicker-Day {
+    border-radius: 0 !important;
+  }
+  .Selectable .DayPicker-Day--start {
+    border-top-left-radius: 50% !important;
+    border-bottom-left-radius: 50% !important;
+  }
+  .Selectable .DayPicker-Day--end {
+    border-top-right-radius: 50% !important;
+    border-bottom-right-radius: 50% !important;
+  }
+`}</style>
+        </Helmet>
+      </div>
+    );
+  }
 }
-
-export default PopUpPicker
