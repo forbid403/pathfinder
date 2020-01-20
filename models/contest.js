1b5cda0d948aaf606f8f9fea881b391a38440fc2
forbid403 <forbid403@naver.com>
2020-01-20 17:30:19 +0900

    [fix] fix issue #13

--- models/contest.js
+++ models/contest.js
@@ -6,7 +6,8 @@ const contestSchema = new Schema({
     title : {type : String, required : true},
     startTime : {type : Date, required : true},
     duration : {type : String, required : true},
-    url : {type : String, required : true, unique : true}
+    url : {type : String, required : true, unique : true},
+    num : {type : Number, default : 0}
 })
 
 module.exports = mongoose.model('contest', contestSchema)
